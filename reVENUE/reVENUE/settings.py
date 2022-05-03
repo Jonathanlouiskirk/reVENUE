ca3916ae4338a36897b85d6eede8ed825b8e530a
jonfaustothen <jonfaustothen@gmail.com>
2022-05-03 19:37:17 -0400

    updating settings data

--- reVENUE/reVENUE/settings.py
+++ reVENUE/reVENUE/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'reVenueapp.apps.ReVenueappConfig',
+    'reVenueapp.apps.RevenueappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
