dbf1ce4c5d66d4fd26503e55373ff27e0d54f9ff
Jonathanlouiskirk <qcpskirk@gmail.com>
2022-05-03 20:10:17 -0400

    viewsdone

--- reVENUE/reVenueapp/views.py
+++ reVENUE/reVenueapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class HomeView(View):
+    def get(self, request):
+        return render(request=request, template_name='home.html')
+        
