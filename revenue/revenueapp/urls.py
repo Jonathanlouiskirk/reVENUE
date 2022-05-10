a5dc495ab37694035130f13d31f4281aa35a3cb7
jonfaustothen <jonfaustothen@gmail.com>
2022-05-10 18:48:08 -0400

    adding styles.css and other changes

--- revenue/revenueapp/urls.py
+++ revenue/revenueapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import HomeView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     # Home
     path('', HomeView.as_view(), name='home'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
