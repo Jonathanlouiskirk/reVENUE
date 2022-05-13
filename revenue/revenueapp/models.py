30fc4251cd64432581ce4aee26bab255e7d4f420
storrer <storrer.chris@gmail.com>
2022-05-12 17:06:58 -0700

    Add Venue class to revenueapp/models.py

--- revenue/revenueapp/models.py
+++ revenue/revenueapp/models.py
@@ -1,3 +1,13 @@
+"""Models module for the revenue app."""
 from django.db import models
 
-# Create your models here.
+class Venue(models.Model):
+    """A class for Venue data."""
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=120)
+    city = models.CharField(max_length=50)
+    # state = models.CharField(max_length=2)
+    # zipcode = models.CharField(max_length=5)
+    # phone = models.CharField(max_length=10)
+    website = models.URLField(max_length=200)
+    image = models.URLField(max_length=200)
