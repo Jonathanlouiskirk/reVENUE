13a7b81fa3623fbe28654a19ebd2fb4bdd8d20d0
storrer <storrer.chris@gmail.com>
2022-06-13 22:30:56 -0700

    Add owner and created_at fields to Review model
    This change includes removing the unique constraint for the venue field on the Review model; more than one review can be created per venue. Also, this commit includes a user.is_authenticated test to the dev.html template as an example of how to check if a user is logged and display content accordingly.

--- revenue/revenueapp/urls.py
+++ revenue/revenueapp/urls.py
@@ -26,10 +26,4 @@ urlpatterns += staticfiles_urlpatterns()
 Django's built in login/logout urls and their URL names are:
 accounts/login/ [name='login']
 accounts/logout/ [name='logout']
-accounts/password_change/ [name='password_change']
-accounts/password_change/done/ [name='password_change_done']
-accounts/password_reset/ [name='password_reset']
-accounts/password_reset/done/ [name='password_reset_done']
-accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
-accounts/reset/done/ [name='password_reset_complete']
 """
