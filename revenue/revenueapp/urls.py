d10e3d4aa3de57e4f69952a35476798ba4623d54
storrer <storrer.chris@gmail.com>
2022-06-18 02:47:39 -0700

    Add login/logout to navbar
    The 'next' parameter for these links controls what page the user is sent to after a successful login/logout; usually this can be set to 'request.path'.

--- revenue/revenueapp/urls.py
+++ revenue/revenueapp/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('venuecreate', VenueCreateView.as_view(), name='venue_create'),
     path('reviewcreate/<int:pk>', ReviewCreateView.as_view(), name='review_create'),
 
-    path('individual_venue/<int:pk>', IndividualVenueView.as_view(), name='individual_venue'),
+    path('individualvenue/<int:pk>', IndividualVenueView.as_view(), name='individual_venue'),
 
     path('reviewupdate/<int:pk>', ReviewUpdateView.as_view(), name='review_update'),
     path('reviewdelete/<int:pk>', ReviewDeleteView.as_view(), name='review_delete'),
