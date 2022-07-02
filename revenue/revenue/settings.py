f4c94db739c1078a2eda4073f30dc36bfd627804
storrer <storrer.chris@gmail.com>
2022-07-02 00:03:57 -0700

    Remove insecure SECRET_KEY from settings.py
    The app now looks for the key in the directory above the project's base directory.

--- revenue/revenue/settings.py
+++ revenue/revenue/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!%ssg6^p)ne6_e!o+qwabh-i4ik!m0rvfbqi%ut&2nz)q)%z$f'
+with open('../secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
