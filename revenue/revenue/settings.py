a0655c4f5ccd16ea9dcf04686203a3a93a603664
storrer <storrer.chris@gmail.com>
2022-07-02 14:38:01 -0700

    Remove asterisk wildcard from ALLOWED_HOSTS

--- revenue/revenue/settings.py
+++ revenue/revenue/settings.py
@@ -26,7 +26,7 @@ with open('../secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*", "18.237.14.147", "ec2-18-237-14-147.us-west-2.compute.amazon.aws.com"]
+ALLOWED_HOSTS = ["18.237.14.147", "ec2-18-237-14-147.us-west-2.compute.amazon.aws.com"]
 
 
 
